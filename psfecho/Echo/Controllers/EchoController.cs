24c42750e26891fd5b16a8bd87d7d2660ac37566
gicherui <gideon.cheruiyot@microsoft.com>
2020-08-16 10:46:40 -0700

    update .devcontainer

--- psfecho/Echo/Controllers/EchoController.cs
+++ psfecho/Echo/Controllers/EchoController.cs
@@ -4,8 +4,6 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Net.Http.Headers;
-using System.Net.Http.Formatting;
-using static System.Net.Http.HttpResponseHeadersExtensions;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -51,14 +49,14 @@ namespace Echo.Controllers
         }
 
         [HttpGet("~/index")]
-        public HttpResponseHeaders Get1()
+        public ContentResult Get1()
         {
-            var resp = new HttpResponseMessage();
 
-            var cookie = new CookieHeaderValue("session-id", "12345");
-            resp.Headers.AddCookies(new List<CookieHeaderValue>() { cookie });
-
-            return resp.Headers;
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = "Calling multiple requests to this endpoint"
+            };
         }
 
 
@@ -76,16 +74,13 @@ namespace Echo.Controllers
         }
 
         [HttpPost("~/index")]
-        public HttpResponseMessage Post2()
+        public ContentResult Post2()
         {
-            var resp = new HttpResponseMessage();
-
-            var cookie = new CookieHeaderValue("session-id", "12345");
-            IEnumerable<CookieHeaderValue> lst = new List<CookieHeaderValue>();
-            lst.Append(cookie);
-
-            resp.Headers.AddCookies(lst);
-            return resp;
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = "Calling multiple requests to this endpoint"
+            };
 
         }
     }
